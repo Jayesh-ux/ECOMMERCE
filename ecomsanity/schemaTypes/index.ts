2edb4d60c0129f2f33c934a805215f43613060fa
Jayesh Singh <hsinghjayesh@gmail.com>
2024-06-16 12:06:49 +0530

    removed sanity

--- ecomsanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import category from "./category";
-import heroimage from "./heroimage";
-import herosection from "./herosection";
-import product from "./product";
-
-
-export const schemaTypes = [product , category, heroimage, herosection,]
