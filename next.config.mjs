8c58d4b86ee62211c050e01d9938b6116cff22aa
Jayesh Singh <hsinghjayesh@gmail.com>
2024-06-16 11:55:28 +0530

    done eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,26 @@
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-        },
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'www.google.com',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+// next.config.mjs
+
+export default {
+  reactStrictMode: true,
+  eslint: {
+    // warn about ESLint errors (add 'ignore' to disable completely)
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+      },
+      // Remove the unnecessary pattern for www.google.com
+    ],
+  },
+};
